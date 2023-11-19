b7f886ff91c34523b2e34448659f4eb95d1d56ec
Daniel Chan <chanjdaniel@gmail.com>
2023-11-19 13:32:21 -0800

    naive bionic reading implemented

--- bionic-bolder.js
+++ bionic-bolder.js
@@ -1,15 +1,64 @@
-const text = document.querySelectorAll('h1, h2, h3, h4, h5, p, td, caption, span, a')
-
-// for (let i = 0; i < text.length; i++) {
-//   text[i].innerHTML = '<b>' + text[i].innerHTML + '</b>'
-//   console.log(text[i])
-// }
+const text = document.querySelectorAll('h1, h2, h3, h4, h5, p, caption, span')
 
 for (let i = 0; i < text.length; i++) {
+    if (text[i].innerHTML.length == 0) {
+        continue
+    }
+    if (text[i].innerHTML[0] == '<') {
+        continue
+    }
+
     text[i].innerHTML = sentenceBionicBolder(text[i].innerHTML)
-    console.log(text[i])
   }
 
 function sentenceBionicBolder(html) {
+    const blacklist = /[^a-zA-Z]/g
+    var boldedHTML = ''
+    var currWordStart = 0
+    var currentlyInTag = false
+    var currentlyInWord = false
+    for (let i = 0; i < html.length; i++) {
+        if (currentlyInTag) {
+            // encountered end of html tag
+            if (html[i] == '>') { currentlyInTag = false }
+            boldedHTML += html[i]
+
+        } else {
+            // encountered beginning of html tag
+            if (html[i] == '<') {
+                currentlyInTag = true
+                boldedHTML += html[i]
+                continue
+            }
+
+            // encountered beginning of word
+            if (html[i].match(blacklist) == null && currentlyInWord == false) {
+                currentlyInWord = true
+                currWordStart = i
+                continue
+            }
+
+            // encountered non-letter character after being within word
+            if (html[i].match(blacklist) != null) {
+                if (currentlyInWord) {
+                    var boldSubstr = html.substring(currWordStart, i)
+                    boldedHTML += wordBionicBolder(boldSubstr)
+                    currentlyInWord = false
+                }
+                boldedHTML += html[i]
+                continue
+            }
+        }
+    }
+
+    return boldedHTML
+}
+
+function wordBionicBolder(word) {
+    var boldedText = word.substring(0,2)
+    var remainingText = word.substring(2,word.length)
     
+    var bionicWord = '<b>' + boldedText + '</b>' + remainingText
+
+    return bionicWord
 }
