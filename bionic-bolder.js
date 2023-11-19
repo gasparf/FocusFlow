3b37d15fe5a56608e6de43382dcb6bcefff21bf9
Daniel Chan <chanjdaniel@gmail.com>
2023-11-19 11:30:07 -0800

    rebase

--- bionic-bolder.js
+++ bionic-bolder.js
@@ -1,6 +1,15 @@
 const text = document.querySelectorAll('h1, h2, h3, h4, h5, p, td, caption, span, a')
 
+// for (let i = 0; i < text.length; i++) {
+//   text[i].innerHTML = '<b>' + text[i].innerHTML + '</b>'
+//   console.log(text[i])
+// }
+
 for (let i = 0; i < text.length; i++) {
-  text[i].innerHTML = '<b>' + text[i].innerHTML + '</b>'
-  console.log(text[i])
+    text[i].innerHTML = sentenceBionicBolder(text[i].innerHTML)
+    console.log(text[i])
+  }
+
+function sentenceBionicBolder(html) {
+    
 }
