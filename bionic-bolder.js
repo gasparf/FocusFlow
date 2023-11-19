a6becc8bd98ca54183acc8af0686abbddea15771
Daniel Chan <chanjdaniel@gmail.com>
2023-11-19 10:55:53 -0800

    good stuff

---
+++ bionic-bolder.js
@@ -0,0 +1,5 @@
+const text = document.querySelectorAll('h1, h2, h3, h4, h5, p, td, caption, span, a')
+
+for (let i = 0; i < text.length; i++) {
+  text[i].innerHTML = '<b>' + text[i].innerHTML + '</b>'
+}
