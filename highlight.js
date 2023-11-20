841da5e0ff9a56bcd0402b527c77a3a2686197c0
enochhsu05 <88109079+enochhsu05@users.noreply.github.com>
2023-11-19 16:21:45 -0800

    added the highlight a phrase after pressing enter feature

---
+++ highlight.js
@@ -0,0 +1,60 @@
+const highlightText = document.querySelectorAll('p')
+i = 0
+strs = {}
+idx = 0
+smallSentence = 20
+document.addEventListener('keypress', (key)=>{
+      if (key.which == 13) {
+        if (idx == 0) {
+          strs = highlightText[i].innerHTML.split(".")
+          len = strs.length
+          if (strs[len - 1].length < 15) {
+            len--
+            strs[len] = "NULL"
+            strs[len-1] += "."
+          }
+          for (let k = 0; k < len; k++) {
+            if (strs[k] != "NULL" && strs[k].length < smallSentence) {
+              if (k != 0) {
+                j = k - 1
+                while (j >= 0) {
+                  if (strs[j] != "NULL") {
+                    strs[j] = strs[j] + strs[k]
+                    break
+                  }
+                  j--
+                }
+              }
+              if (len > 1) {
+                strs[k] = "NULL"
+              }
+            }
+            if (k < len - 1) {
+              strs[k] += "."
+            }
+          }
+        }
+        str = ""
+        for (let k = 0; k < len; k++) {
+          while (k < len && strs[k] == "NULL") {
+            if (idx == k) idx++
+            k++
+          }
+          if (k < len && strs[k] != "NULL") {
+            if (k == idx) {
+              strs[k] = '<mark>' + strs[k] + '</mark>'
+              str += strs[k]
+            } else {
+              str += strs[k]
+            }
+            console.log(str)
+          }
+        }
+        idx++
+        highlightText[i].innerHTML = str
+        if (idx >= len) {
+          idx = 0
+          i++
+        }
+      }
+});
