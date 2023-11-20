b71f5453114faee133b5454f1689540f70a52f71
enochhsu05 <88109079+enochhsu05@users.noreply.github.com>
2023-11-19 17:53:23 -0800

    added some comments

--- highlight.js
+++ highlight.js
@@ -1,65 +1,69 @@
 highlightText = document.querySelectorAll('p')
+
+// makes all "..." into "…"
 for (let k = 0; k < highlightText.length; k++) {
     if (highlightText[k].innerHTML.indexOf("...") != -1) {
         highlightText[k].innerHTML = highlightText[k].innerHTML.replaceAll("...", "…")
     }
 }
-i = 0
-strs = {}
-idx = 0
-smallSentence = 20
+
+i = 0 // the index of the current chunk of text
+strs = {} // the list containing every sentence in the current chunk of text
+idx = 0 // the index of the current sentence in the current chunk of test
+smallSentence = 20 // what makes a sentence small
 document.addEventListener('keypress', (key)=>{
+    // only include <p> and not <p anything else>
     while (i < highlightText.length && highlightText[i].className != "") {
         i++
     }
+    
     if (key.which == 13 && i < highlightText.length) {
         if (idx == 0) {
             strs = highlightText[i].innerHTML.split(".")
             len = strs.length
-            if (strs[len - 1].length < 15) {
-            len--
-            strs[len] = "NULL"
-            strs[len-1] += "."
-            }
+            // if (strs[len - 1].length < 15) {
+            //     len--
+            //     strs[len] = "NULL"
+            //     strs[len-1] += "."
+            // }
             for (let k = 0; k < len; k++) {
-            if (strs[k] != "NULL" && strs[k].length < smallSentence) {
-                if (k != 0) {
-                j = k - 1
-                while (j >= 0) {
-                    if (strs[j] != "NULL") {
-                    strs[j] = strs[j] + strs[k]
-                    break
+                if (strs[k] != "NULL" && strs[k].length < smallSentence) {
+                    if (k != 0) {
+                        j = k - 1
+                        while (j >= 0) {
+                            if (strs[j] != "NULL") {
+                            strs[j] = strs[j] + strs[k]
+                            break
+                            }
+                            j--
+                        }
                     }
-                    j--
+                    if (len > 1) strs[k] = "NULL"
                 }
+                if (k < len - 1 && strs[k] != "NULL") {
+                    strs[k] += "."
                 }
-                if (len > 1) {
-                strs[k] = "NULL"
-                }
-            }
-            if (k < len - 1) {
-                strs[k] += "."
-            }
+                console.log(strs[k])
             }
         }
         str = ""
         for (let k = 0; k < len; k++) {
             while (k < len && strs[k] == "NULL") {
-            if (idx == k) idx++
-            k++
+                if (idx == k) idx++
+                k++
             }
             if (k < len && strs[k] != "NULL") {
-            if (k == idx) {
-                strs[k] = '<mark>' + strs[k] + '</mark>'
-                str += strs[k]
-            } else {
-                str += strs[k]
-            }
+                if (k == idx) {
+                    strs[k] = '<mark>' + strs[k] + '</mark>'
+                    str += strs[k]
+                } else {
+                    str += strs[k]
+                }
             }
         }
         idx++
         highlightText[i].innerHTML = str
-        if (idx >= len) {
+        if (idx >= len || (strs[idx] == "NULL" && idx == len - 1)) {
             idx = 0
             i++
         }
